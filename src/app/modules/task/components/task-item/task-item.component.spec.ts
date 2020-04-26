547a7d71159d2b5ea81c2764e46e774d09430e71
gregorio.silva <sv4lrmrpfvope7xf27igldjedqbcsjf55l22yk7nx5apaj434k7a>
2020-04-26 15:52:42 -0300

    todos os testes de eventos 100%

--- src/app/modules/task/components/task-item/task-item.component.spec.ts
+++ src/app/modules/task/components/task-item/task-item.component.spec.ts
@@ -1,4 +1,4 @@
-import {render, RenderResult, fireEvent} from '@testing-library/angular';
+import {render, RenderResult, fireEvent, userEvent} from '@testing-library/angular';
 import {ComponentFixture} from '@angular/core/testing';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -14,6 +14,61 @@ describe('TaskItemComponent', () => {
   let renderResult: RenderResult<TaskItemComponent>;
   let fixture: ComponentFixture<TaskItemComponent>;
   let component: TaskItemComponent;
+
+  const saveOnEnterOrBlur = (event, options = {}) => {
+    const {getByTitle, getByPlaceholderText} = renderResult;
+    const updateTaskMock = jest.fn((task: Task) => task);
+    const newLabel = 'Tarefa atualizada';
+
+    component.task = taskTestBuilder.build();
+    component.updateTask.emit = updateTaskMock;
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const editButton = getByTitle('Editar');
+
+    editButton.click();
+
+    fixture.detectChanges();
+
+    const inputElement = getByPlaceholderText('Digite a descrição da tarefa...');
+
+    userEvent.type(inputElement, newLabel);
+    event(inputElement, options);
+
+    expect(updateTaskMock).toHaveBeenCalledTimes(1);
+    expect(updateTaskMock).not.toHaveBeenCalledWith(component.task);
+    expect(updateTaskMock.mock.results[0].value).toEqual({...component.task, label: newLabel});
+  }
+
+  const dontSaveOnEnterOrBlur = (event, options = {}) => {
+    const {getByTitle, getByPlaceholderText} = renderResult;
+    const editTaskMock = jest.fn((task: Task) => task);
+    let editCallCounter = 0;
+
+    component.task = taskTestBuilder.build();
+    component.editTask.emit = editTaskMock;
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const editButton = getByTitle('Editar');
+
+    editButton.click();
+    editCallCounter++;
+
+    fixture.detectChanges();
+
+    const inputElement = getByPlaceholderText('Digite a descrição da tarefa...');
+
+    event(inputElement, options);
+    editCallCounter++;
+
+    expect(editTaskMock).toHaveBeenCalledTimes(editCallCounter);
+    expect(editTaskMock).toHaveBeenCalledWith(null);
+    expect(editTaskMock.mock.results[editCallCounter - 1].value).toEqual(null);
+  }
 
   beforeEach(async () => {
     renderResult = await render(TaskItemComponent, {
@@ -110,10 +165,18 @@ describe('TaskItemComponent', () => {
   });
 
   it('should save changes on keydown Enter', () => {
-    expect(false).toBeTruthy();
+    saveOnEnterOrBlur(fireEvent.keyDown, { key: 'Enter', code: 'Enter' });
   });
 
-  it('should save changes on input blur', () => {
-    expect(false).toBeTruthy();
+  it('should save changes on blur', () => {
+    saveOnEnterOrBlur(fireEvent.blur);
+  });
+
+  it('should not save on Enter', () => {
+    dontSaveOnEnterOrBlur(fireEvent.keyDown, { key: 'Enter', code: 'Enter' });
+  });
+
+  it('should not save on blur', () => {
+    dontSaveOnEnterOrBlur(fireEvent.blur);
   });
 });
