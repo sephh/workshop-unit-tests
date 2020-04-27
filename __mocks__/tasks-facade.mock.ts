ef642153ff82dcb5c92663090f5a09d742460774
gregorio.silva <sv4lrmrpfvope7xf27igldjedqbcsjf55l22yk7nx5apaj434k7a>
2020-04-27 06:02:36 -0300

    initial tests and tasks-facade.mock.ts

---
+++ __mocks__/tasks-facade.mock.ts
@@ -0,0 +1,36 @@
+import {BehaviorSubject} from 'rxjs';
+import {Task} from 'task-state';
+
+export class TasksFacadeMock {
+  tasks$ = new BehaviorSubject<Task[]>([]);
+  loading$ = new BehaviorSubject<boolean>(false);
+  selectedTaskId$ = new BehaviorSubject<string>('');
+
+  getTasks() {
+    this.tasks$.next([]);
+  }
+
+  createTask(task: Task) {
+    const currentTasks = this.tasks$.getValue();
+    this.tasks$.next([...currentTasks, task]);
+  }
+
+  selectTask(id: string) {
+    this.selectedTaskId$.next(id);
+  }
+
+  updateTask(task: Task) {
+    const currentTasks = this.tasks$.getValue();
+    const updatedTasks = currentTasks.map(t => (t.id === task.id ? task : t));
+    this.tasks$.next(updatedTasks);
+  }
+
+  deleteTask(task: Task) {
+    const currentTasks = this.tasks$.getValue();
+    this.tasks$.next(currentTasks.filter(t => t.id !== task.id));
+  }
+
+  flushTasks(tasks: Task[]) {
+    this.tasks$.next(tasks);
+  }
+}
