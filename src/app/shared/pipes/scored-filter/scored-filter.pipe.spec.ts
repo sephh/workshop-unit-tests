b861db8b2a7a4228be1e583810f5a65d1542fd9c
gregorio.silva <sv4lrmrpfvope7xf27igldjedqbcsjf55l22yk7nx5apaj434k7a>
2020-04-28 08:09:26 -0300

    teste scoredFilter pipe

--- src/app/shared/pipes/scored-filter/scored-filter.pipe.spec.ts
+++ src/app/shared/pipes/scored-filter/scored-filter.pipe.spec.ts
@@ -1,8 +1,62 @@
 import { ScoredFilterPipe } from './scored-filter.pipe';
 
+const mockData = [
+  {
+    label: 'abacate',
+    type: 'bonito'
+  },
+  {
+    label: 'abacaxi',
+    type: 'maduro'
+  },
+  {
+    label: 'banana',
+    type: 'verde'
+  },
+  {
+    label: 'caqui',
+    type: 'bonito'
+  },
+  {
+    label: 'Jaboticaba',
+    type: 'maduro'
+  },
+  {
+    label: 'limão',
+    type: 'azedo'
+  },
+  {
+    label: 'picles',
+    type: 'azedo'
+  }
+];
+
 describe('ScoredFilterPipe', () => {
   it('create an instance', () => {
     const pipe = new ScoredFilterPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should filter by key value or values', ()=>{
+    const pipe = new ScoredFilterPipe();
+
+    const expected1 = pipe.transform(mockData, 'bonito', 'type');
+    const expected2 = pipe.transform(mockData, 'bo', ['label', 'type']);
+
+    expect(expected1).toEqual(mockData.filter(d => d.type === 'bonito'));
+    expect(expected2).toEqual([
+      {
+        label: 'abacate',
+        type: 'bonito'
+      },
+      {
+        label: 'caqui',
+        type: 'bonito'
+      },
+      {
+        label: 'Jaboticaba',
+        type: 'maduro'
+      }
+    ]);
   });
 });
